a64b24503ca4a9775d9636010390ea6dcc613ff3
Hata <141723500+h4ta@users.noreply.github.com>
2024-10-30 06:01:12 +0000

    タイマーを追加

--- src/component/Guide.tsx
+++ src/component/Guide.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
 type Props = {
   isGameClear: boolean;
@@ -23,14 +23,53 @@ export const Guide = (props: Props) => {
     setLeftFlagNum,
   } = props;
 
+  // クリア時間を計測する
+  const [playingTime, setPlayingTime] = useState<number>(0);
+  const intervalId = useRef<number | null>(null);
+
+  const startTimer = () => {
+    if (intervalId.current) return;
+    intervalId.current = window.setInterval(
+      () => setPlayingTime((n) => n + 1),
+      1000
+    );
+  };
+
+  const stopTimer = () => {
+    if (!intervalId.current) return;
+    clearInterval(intervalId.current);
+    intervalId.current = null;
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    setPlayingTime(0);
+  };
+
+  useEffect(() => {
+    if (isGameNow) {
+      startTimer();
+    }
+  }, [isGameNow]);
+
   return (
     <>
       {isGameNow ? (
         <>
           <div>
+            <div>
+              time {Math.floor(playingTime / 60)} : {playingTime % 60}
+            </div>
             <div>🚩: {leftFlagNum}</div>
           </div>
-          <button onClick={restart}>restart</button>
+          <button
+            onClick={() => {
+              restart();
+              resetTimer();
+            }}
+          >
+            restart
+          </button>
           <button onClick={() => setIsFlaggingNow((state) => !state)}>
             {isFlaggingNow ? "change to open" : "change to flag"}
           </button>
@@ -38,7 +77,14 @@ export const Guide = (props: Props) => {
       ) : (
         <div>Press any space to start</div>
       )}
-      {isGameClear && <div>Game Clear!!</div>}
+      {isGameClear && (
+        <>
+          <div>Game Clear!!</div>
+          <div>
+            Your clear time {Math.floor(playingTime / 60)} : {playingTime % 60}
+          </div>
+        </>
+      )}
       {isGameOver && <div>Game Over</div>}
     </>
   );
